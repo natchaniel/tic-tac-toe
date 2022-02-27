fc4b83214cba76ebc649c46817af8d38b931f78c
natchaniel <nathan-soft1@outlook.com>
2022-02-27 11:08:48 -0600

    you can win! (kinda)
    added win condition, but diagonals don't grant a victory. :(

--- script.js
+++ script.js
@@ -3,8 +3,19 @@ const game     = document.getElementById('game');
 const header   = document.getElementById('header');
 const messages = document.getElementById('messages');
 
+const colsrows = [
+	document.querySelectorAll('.one'),	 // row 1
+	document.querySelectorAll('.two'),	 // row 2
+	document.querySelectorAll('.three'), // row 3
+	document.querySelectorAll('.a'),  // column 1
+	document.querySelectorAll('.b'),  // column 2
+	document.querySelectorAll('.c'),  // column 3
+];
+
 let username = 'nobody'
 let currentTurn = 'none'
+let turns = 0
+let winner = 'nobody'
 
 function toggleDisplay() {
 	joinDiv.classList.toggle('hidden');
@@ -19,6 +30,7 @@ function swapTurns() {
 			currentTurn = 'X';
 			break;
 	}
+	turns++;
 }
 function newMessage(type, txt, clear) {
 	const msg = document.createElement('li');
@@ -43,6 +55,9 @@ document.querySelector('#singleplayerForm').addEventListener('submit', e => {
 	toggleDisplay();
 
 	if (currentTurn === 'none') { currentTurn = 'X' }
+	turns++;
+	newMessage('server', `Playing offline. ${currentTurn}'s turn`, true);
+	newMessage('server', `Turn ${turns}`, false);
 });
 
 document.querySelectorAll('div#gameBoard button').forEach(btn => {
@@ -51,6 +66,34 @@ document.querySelectorAll('div#gameBoard button').forEach(btn => {
 			btn.textContent = currentTurn;
 			swapTurns();
 			newMessage('server', `${currentTurn}'s turn`, true);
+			newMessage('server', `Turn ${turns}`, false);
+			checkForWinner();
 		};
 	});
 });
+function checkForWinner() {
+	if ( turns === 10 ) {
+		console.log('no winner...')
+		document.querySelectorAll('button.game').forEach(e => {
+			e.disabled = true;
+		});
+	}
+	colsrows.forEach(btns => {
+		let Xs = 0;
+		let Os = 0;
+		btns.forEach(btn => {
+			if ( btn.innerHTML === 'X' ) { Xs++ };
+			if ( btn.innerHTML === 'O' ) { Os++ };
+		});
+		if ( Xs === 3 || Os === 3 ) {
+			// ADD CODE FOR WHEN IT'S WON
+			if ( Xs === 3 ) { winner = 'X' } else { winner = 'O' }
+			console.log(`Congrats, ${winner}! You won!`)
+			document.querySelectorAll('button.game').forEach(e => {
+				e.disabled = true;
+			});
+		} else {
+			// check for diagonals
+		}
+	});
+}
